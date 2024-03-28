9b4934c8f77e28d4abcf0b65d7e1d62811f381e4
chinmayee behera <chinmayeebehera2020@gmail.com>
2024-03-28 14:40:03 +0530

    confilct errror

--- src/screens/auth/Login.tsx
+++ src/screens/auth/Login.tsx
@@ -1,5 +1,5 @@
-import { useNavigation } from '@react-navigation/native';
-import { NativeStackNavigationProp } from '@react-navigation/native-stack';
+import {useNavigation} from '@react-navigation/native';
+import {NativeStackNavigationProp} from '@react-navigation/native-stack';
 import AnimatedLottieView from 'lottie-react-native';
 import {
   Box,
@@ -16,16 +16,16 @@ import {
   Heading,
   Spinner,
 } from 'native-base';
-import React, { useMemo, useState } from 'react';
-import { useForm } from 'react-hook-form';
-import { ImageBackground, Linking, useWindowDimensions } from 'react-native';
-import { IMAGES } from '~/assets';
-import { LOTTI } from '~/assets/animations';
-import { AppInput, Btn, Content, CountryPicker } from '~/components/core';
-import AppIcon, { IconProps } from '~/components/core/AppIcon';
-import { useAuth, useMutation, useSwrApi } from '~/hooks';
-import { PublicRoutesTypes } from '~/routes';
-import { COLORS, HEIGHT } from '~/styles';
+import React, {useMemo, useState} from 'react';
+import {useForm} from 'react-hook-form';
+import {ImageBackground, Linking, useWindowDimensions} from 'react-native';
+import {IMAGES} from '~/assets';
+import {LOTTI} from '~/assets/animations';
+import {AppInput, Btn, Content, CountryPicker} from '~/components/core';
+import AppIcon, {IconProps} from '~/components/core/AppIcon';
+import {useAuth, useMutation, useSwrApi} from '~/hooks';
+import {PublicRoutesTypes} from '~/routes';
+import {COLORS, HEIGHT} from '~/styles';
 import {
   GoogleSignin,
   statusCodes,
@@ -49,27 +49,28 @@ export default function Login(): JSX.Element {
   const [secureTextEntry, setSecureTextEntry] = useState(true);
   const [visible, setVisible] = useState(false);
   const [isPhone, setIsPhone] = useState(false);
+  const {mutation: gmail, isLoading: isGmailValidating} = useMutation();
   let objData: any = {};
-  const { navigate } =
+  const {navigate} =
     useNavigation<NativeStackNavigationProp<PublicRoutesTypes>>();
-  const { height } = useWindowDimensions();
+  const {height} = useWindowDimensions();
   const {
     control,
     handleSubmit,
-    formState: { errors },
+    formState: {errors},
   } = useForm<FormData>();
 
   const {setUser, getUser, setToken} = useAuth();
   const {mutation: login, isLoading} = useMutation();
   const {mutation: gLogin} = useMutation();
 
-  const { data, error, isValidating } = useSwrApi(`auth/google/select-profile`);
+  const {data, error, isValidating} = useSwrApi(`auth/google/select-profile`);
 
   console.log(data, 'data');
 
   const handleLogin = async (formData: FormData) => {
     try {
-      const { mobile } = formData;
+      const {mobile} = formData;
       // console.log('Mobile Number:', mobile);
       // console.log('Mobile Number:', selectedCountry.name);
       // console.log('Mobile Number:', selectedCountry.phone);
@@ -81,8 +82,8 @@ export default function Login(): JSX.Element {
         },
       };
 
-      324969
-      389516
+      324969;
+      389516;
       const res = await login(`auth/generate-otp`, {
         isAlert: true,
         body: objData,
@@ -167,7 +168,7 @@ export default function Login(): JSX.Element {
         key: 'mobile',
         label: undefined,
         placeholder: 'Enter your mobile number',
-        icon: { IoniconsName: 'call', color: 'gray' },
+        icon: {IoniconsName: 'call', color: 'gray'},
         rules: {
           required: 'Mobile number is required',
           pattern: {
@@ -234,7 +235,7 @@ export default function Login(): JSX.Element {
   );
 
   return (
-    <ScrollView contentContainerStyle={{ flexGrow: 1 }}>
+    <ScrollView contentContainerStyle={{flexGrow: 1}}>
       <Box
         zIndex={1}
         bg={{
@@ -247,12 +248,12 @@ export default function Login(): JSX.Element {
         flex={1}
         position="relative">
         <ImageBackground
-          style={{ flex: 1, height: 450 }}
+          style={{flex: 1, height: 450}}
           borderRadius={5}
           resizeMode="cover"
           source={IMAGES.FIRST}
           alt="Logo"
-          imageStyle={{ opacity: 0.2, backgroundColor: 'blue' }}>
+          imageStyle={{opacity: 0.2, backgroundColor: 'blue'}}>
           <Image
             source={IMAGES.SECOND}
             resizeMode={'contain'}
@@ -372,19 +373,19 @@ export default function Login(): JSX.Element {
                   ))}
                 </Box>
 
-              <Box m={4}>
-                <Btn
-                  bg={COLORS.PRIMARY}
-                  _text={{color: 'white', fontSize: 'sm'}}
-                  onPress={handleSubmit(handleLogin)}
-                  shadow={0.8}>
-                  {isLoading ? (
-                    <Spinner size={'sm'} color={'white'} />
-                  ) : (
-                    <Heading fontSize={15} py={1} color={'white'}>
-                      Sign In
-                    </Heading>
-                  )}
+                <Box m={4}>
+                  <Btn
+                    bg={COLORS.PRIMARY}
+                    _text={{color: 'white', fontSize: 'sm'}}
+                    onPress={handleSubmit(handleLogin)}
+                    shadow={0.8}>
+                    {isLoading ? (
+                      <Spinner size={'sm'} color={'white'} />
+                    ) : (
+                      <Heading fontSize={15} py={1} color={'white'}>
+                        Sign In
+                      </Heading>
+                    )}
 
                     <AppIcon FeatherName="log-in" color={'white'} size={20} />
                   </Btn>
@@ -394,35 +395,64 @@ export default function Login(): JSX.Element {
                   - Or -
                 </Text>
 
-              <Pressable
-                _pressed={{opacity: 0.8}}
-                w={'92%'}
-                py={1.5}
-                borderColor={'blue.800'}
-                borderRadius={6}
-                bg={'white'}
-                alignSelf={'center'}
-                justifyContent={'center'}
-                alignItems={'center'}
-                borderWidth={0.3}>
-                <HStack
-                  alignItems={'center'}
+                <Pressable
+                  _pressed={{opacity: 0.8}}
+                  w={'92%'}
+                  py={1.5}
+                  borderColor={'blue.800'}
+                  borderRadius={6}
+                  bg={'white'}
+                  alignSelf={'center'}
                   justifyContent={'center'}
-                  space={3}>
-                  <Image
-                    source={IMAGES.GOOGLE}
-                    resizeMode={'contain'}
-                    h="5"
-                    w="6"
-                    bg={'transparent'}
-                    alt="Logo"
-                  />
-                  <Heading fontSize={15} py={1} color={'black'}>
-                    Login With Google
-                  </Heading>
-                </HStack>
-              </Pressable>
-            </VStack>
+                  alignItems={'center'}
+                  borderWidth={0.3}>
+                  <HStack
+                    alignItems={'center'}
+                    justifyContent={'center'}
+                    space={3}>
+                    <Image
+                      source={IMAGES.GOOGLE}
+                      resizeMode={'contain'}
+                      h="5"
+                      w="6"
+                      bg={'transparent'}
+                      alt="Logo"
+                    />
+                    <Heading fontSize={15} py={1} color={'black'}>
+                      Login With Google
+                    </Heading>
+                  </HStack>
+                </Pressable>
+              </VStack>
+            )}
+            <Pressable
+              _pressed={{opacity: 0.8}}
+              w={'92%'}
+              py={1.5}
+              onPress={() => {
+                setIsPhone(!isPhone);
+              }}
+              borderColor={'blue.800'}
+              borderRadius={6}
+              bg={'white'}
+              alignSelf={'center'}
+              justifyContent={'center'}
+              alignItems={'center'}
+              borderWidth={0.3}>
+              <HStack alignItems={'center'} justifyContent={'center'} space={3}>
+                <Image
+                  source={IMAGES.GOOGLE}
+                  resizeMode={'contain'}
+                  h="5"
+                  w="6"
+                  bg={'transparent'}
+                  alt="Logo"
+                />
+                <Heading fontSize={15} py={1} color={'black'}>
+                  {`Login With ${isPhone ? 'Phone' : 'Gmail'}`}
+                </Heading>
+              </HStack>
+            </Pressable>
 
             {/* <Box mt={5} alignItems={'center'}>
               <HStack>
@@ -443,7 +473,7 @@ export default function Login(): JSX.Element {
                 </Content>
                 <Pressable
                   _pressed={{ opacity: 0.6 }}
-                // onPress={() => Linking.openURL('https://yard-ecommerce-web.vercel.app/privacy-policy')}
+                onPress={() => Linking.openURL('https://yard-ecommerce-web.vercel.app/privacy-policy')}
                 >
                   <Content fontSize={12} weight="400" color="red.500" underline>
                     Privacy Policy
